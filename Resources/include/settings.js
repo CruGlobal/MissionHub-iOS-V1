79bb17c8c91f0ebb53e16c199cbe87944b789c93
Chris Roemmich <chris.roemmich@cojourners.com>
2011-06-19 13:20:50 -0400

    empty settings file

---
+++ Resources/include/settings.js
@@ -0,0 +1,18 @@
+/*!
+ * MissionHub Settings
+ * http://hub.ccci.us
+ *
+ * Copyright 2011, Campus Crusade for Christ International
+ *  
+ * Description: Settings for MissionHub
+ * Author: Chris Roemmich <chris.roemmich@cojourners.com>
+ * Date: Thu, 16 Jun 2011 13:25:35 -0400
+ */
+
+var Settings = {};
+
+Settings.oauth_url = '';
+Settings.oauth_client_id = '';
+Settings.oauth_client_secret = '';
+Settings.oauth_scope = '';
+Settings.api_url = '';
