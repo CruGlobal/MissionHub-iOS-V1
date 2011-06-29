7d9af2596594e1f92b97370e85ba21f54b48a7de
Chris Roemmich <chris.roemmich@cojourners.com>
2011-06-29 12:36:15 -0400

    null config

--- Resources/mh/config/config.js
+++ Resources/mh/config/config.js
@@ -1,22 +1,16 @@
 (function() {
 	
-	mh.config = {
-		base_url: 'http://hub.ccci.us',
-		oauth_url: 'http://hub.ccci.us/oauth',
-		oauth_client_id: '4',
-		oauth_client_secret: 'a0f7de3c353d69c61d4c8a94b338c073d09ebd381bcb4a3b304c59cf4b15bfd5',
-		oauth_scope: 'userinfo,contacts,contact_assignment,followup_comments',
-		api_url: 'http://hub.ccci.us/api/v1',
-		
-		// oauth_url: 'http://test.ccci.us:8881/oauth',
-		// oauth_client_id: '4',
-		// oauth_client_secret: 'a0f7de3c353d69c61d4c8a94b338c073d09ebd381bcb4a3b304c59cf4b15bfd5',
-		// oauth_scope: 'userinfo,contacts,contact_assignment,followup_comments',
-		// api_url: 'http://test.ccci.us:8881/api/v1',
+	mh.config = {	
+		base_url: '',
+		oauth_url: '',
+		oauth_client_id: '',
+		oauth_client_secret: '',
+		oauth_scope: '',
+		api_url: '',
 		
 		cache: {
 			disable: false,
-			cache_expiration_interval: 60
+			cache_expiration_interval: 15
 		}
 	};
 	
